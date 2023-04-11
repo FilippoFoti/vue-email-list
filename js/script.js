aa853f22ade5d4155f5b3f9b4539c2e8fa8080fe
FilippoFoti <filippofoti7@gmail.com>
2023-04-11 14:29:24 +0200

    Exercise completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,23 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
+            email: []
+        }
+    },
+    methods: {
+        emailGenerator() {
+            const emailArray = [];
 
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get ("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then ((resp) => {
+                        emailArray.push(resp.data.response);
+                        if (emailArray.length === 10) {
+                            this.email = emailArray;
+                        }
+                    })
+            }
         }
     }
-}).mount("#app");
+}).mount ("#app");
